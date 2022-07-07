17cc6be094f196c79117ef17f02985bc01e7806f
Brennen Shtern <brennen.shtern@gmail.com>
2022-07-07 12:07:19 -0500

    commit tree e050603fded423829dd389692e6275207e89efd1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, events) {
+    let message = [];
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${events} gift!`)
+    }
+    return message;
+}
+
+function countDown(int) {
+    let i = 10
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
